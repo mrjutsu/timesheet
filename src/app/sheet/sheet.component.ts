0becf6cc90b7b9dad658e0138844daf8f6f691cc
David Rosillo <drr538@gmail.com>
2019-05-29 11:27:47 -0500

    Created functionality to calculate time

--- src/app/sheet/sheet.component.ts
+++ src/app/sheet/sheet.component.ts
@@ -1,35 +1,59 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-sheet',
   templateUrl: './sheet.component.html',
   styleUrls: ['./sheet.component.css']
 })
-export class SheetComponent implements OnInit {
+export class SheetComponent implements OnInit, DoCheck {
 
-  rows = [
+  rows: any = [
     { date: undefined,
       start_time: { hour: 12, minute: 0 },
       end_time: { hour: 12, minute: 0 },
       regular_hours: undefined,
-      total_hours: undefined
+      total_hours: undefined,
+      hours: ''
     }
   ];
 
-  total_hours = 0;
-
-  // @Input() rows = [
-  //   { date: undefined,
-  //     start_time: undefined,
-  //     end_time: undefined,
-  //     regular_hours: undefined,
-  //     total_hours: undefined
-  //   }
-  // ];
+  total_hours: any = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngDoCheck(){
+    for (let row of this.rows) {
+      if (row.date && row.start_time && row.end_time){
+        this.calculateRow(row);
+      }
+    }
+  }
+
+  calculateRow(row){
+    if (row.date && row.start_time && row.end_time) {
+      let start = moment({ year: row.date.year, month: row.date.month, day: row.date.day, hour: row.start_time.hour, minute: row.start_time.minute});
+      let end = moment({ year: row.date.year, month: row.date.month, day: row.date.day, hour: row.end_time.hour, minute: row.end_time.minute});
+
+      let diff: Number = end.diff(start,'hours',true);
+      row.total_hours = diff;
+      if (diff % 1 == 0){
+        row.hours = row.total_hours;
+      }else{
+        row.hours = diff.toFixed(2);
+      }
+      this.calculateTime();
+    }
+  }
+
+  calculateTime(){
+    this.total_hours = 0;
+    for (let row of this.rows) {
+      this.total_hours += row.total_hours;
+    }
+    this.total_hours = this.total_hours.toFixed(2);
   }
 
   addRow(): void {
